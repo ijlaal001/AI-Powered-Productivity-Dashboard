0e8fc7dbe918c29278b3b83753785422fb8f0f81
ijlaal001 <ijlaalakhtar@gmail.com>
2025-04-26 17:05:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHrbrU8BWFB4JEvIsvVquNEEU2EYf3uck",
+  authDomain: "productivity-dashboard-with-ai.firebaseapp.com",
+  projectId: "productivity-dashboard-with-ai",
+  storageBucket: "productivity-dashboard-with-ai.firebasestorage.app",
+  messagingSenderId: "10807938376",
+  appId: "1:10807938376:web:efdda234b5b24821aab77d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, signInWithPopup, signOut, onAuthStateChanged };
