a4ed04dc863f3a26549f692460f2fd5e23cc16d0
ijlaal001 <ijlaalakhtar@gmail.com>
2025-04-26 17:12:06 +0530

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,9 @@
+// clock.js
+function updateClock() {
+  const now = new Date();
+  const clock = document.getElementById('clock');
+  clock.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
