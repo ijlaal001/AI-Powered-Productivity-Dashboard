b7bcacb74f605f5086804a3370705aa14b56fa2c
ijlaal001 <ijlaalakhtar@gmail.com>
2025-04-26 18:18:54 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,18 @@
-// No imports here because compat SDKs are included via <script> in HTML
+// Using compat version for Firebase 9
+import firebase from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHrbrU8BWFB4JEvIsvVquNEEU2EYf3uck",
   authDomain: "productivity-dashboard-with-ai.firebaseapp.com",
   projectId: "productivity-dashboard-with-ai",
-  storageBucket: "productivity-dashboard-with-ai.firebasestorage.app",
+  storageBucket: "productivity-dashboard-with-ai.appspot.com",
   messagingSenderId: "10807938376",
   appId: "1:10807938376:web:efdda234b5b24821aab77d"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Initialize Auth service
+const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
